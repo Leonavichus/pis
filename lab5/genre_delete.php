20c46c424ac714381a2ef739a459a74db78a0b76
Yoshi <dleonavichus@mail.ru>
2021-12-01 17:35:24 +0300

    pis

---
+++ lab5/genre_delete.php
@@ -0,0 +1,27 @@
+<?php
+// Modules
+require 'db.php';
+require 'header.php';
+require 'footer.php';
+// Update authors
+if (isset($_POST['id'])) {
+    $id = $_POST['id'];
+    $genre = R::load('genre', $id);
+    R::trash($genre);
+}
+?>
+
+<div class="card-header">
+    <h2>Delete genre(Задание№3)</h2>
+</div>
+<div class="card-body">
+    <form method="post">
+        <div class="form-group">
+            <label for="id">Id</label>
+            <input type="text" name="id" id="id" class="form-control">
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-info">Select</button>
+        </div>
+    </form>
+</div>
