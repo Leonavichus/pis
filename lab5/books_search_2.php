0a5f4073b4825dca46bddf814385763a56368b17
Yoshi <dleonavichus@mail.ru>
2021-12-03 16:21:09 +0300

    pis

--- lab5/books_search_2.php
+++ lab5/books_search_2.php
@@ -9,12 +9,24 @@ $books = R::findAll('books');
 <div class="container">
     <div class="card mt-5">
         <div class="card-body">
-            <?php foreach ($books as $books) : ?>
-                <ul>
-                    <li><?= $books->title; ?></li>
-                    <li><?= $books->yearwriting; ?></li>
-                </ul>
-            <?php endforeach; ?>
+            <table class="table table-bordered">
+                <tr>
+                    <th>Marker</th>
+                    <th>Title</th>
+                    <th>Year_Writing</th>
+                </tr>
+                <?php foreach ($books as $books) : ?>
+                    <tr>
+                        <th>
+                            <ul>
+                                <li></li>
+                            </ul>
+                        </th>
+                        <td><?= $books->title; ?></td>
+                        <td><?= $books->yearwriting; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
         </div>
     </div>
 </div>
